4dadb1e25f2c1d48ee4f7f7529049d7c9d4dedc5
Rez1ce <zaspanec666@yandex.ru>
2021-08-15 02:33:52 +0300

    SQLAlchemy

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from cred import username, passwd, address, port, db_name
+import datetime
 
 app = Flask(__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://'+username+':'+passwd+'@'+address+':'+port+'/'+db_name
+#app.config ['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://'+username+':'+passwd+'@'+address+':'+port+'/'+db_name
 
-db = SQLAlchemy(app)
-class users(db.Model):
-    user_id = db.Column('id', db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    middle_name = db.Column(db.String(100))
-    bornDate = db.Column(db.String(100))
-    gender = db.Column(db.String(100))
+engine = create_engine('postgresql+psycopg2://'+username+':'+passwd+'@'+address+':'+port+'/'+db_name)
+base = declarative_base()
+#db = SQLAlchemy(app)
 
-class users_info(db.Model):
-    user_id = db.Column('id', db.Integer, primary_key = True)
-    education = db.Column(db.String(100))
-    comment = db.Column(db.String(100))
-    citizenship = db.Column(db.String(100))
+class users(base):
+    __tablename__ = 'users'
+    user_id = Column('id',Integer, primary_key = True)
+    name = Column(String)
+    middle_name = Column(String)
+    bornDate = Column(Date)
+    gender = Column(String)
+
+class users_info(base):
+    __tablename__ = 'users_info'
+    user_id = Column('id', Integer, primary_key = True)
+    education = Column(String)
+    comment = Column(String)
+    citizenship = Column(String)
+#class users(db.Model):
+#    user_id = db.Column('id', db.Integer, primary_key = True)
+#    name = db.Column(db.String())
+#    middle_name = db.Column(db.String())
+#    bornDate = db.Column(db.Date)
+#    gender = db.Column(db.String())
+#
+#class users_info(db.Model):
+#    user_id = db.Column('id', db.Integer, primary_key = True)
+#    education = db.Column(db.String())
+#    comment = db.Column(db.String())
+#    citizenship = db.Column(db.String())
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        
         username = request.form.get('userName')
         surname = request.form.get('userSurname')
         gender = request.form.get('gender')
@@ -29,17 +51,27 @@ def index():
         dateBorn = request.form.get('dateBorn')
         education = request.form.get('education')
         comment = request.form.get('comment')
+        education = education.encode('cp1252')
+        dateBorn = datetime.datetime.strptime(dateBorn, '%Y-%m-%d').date()
 
         new_user = users(name=username, middle_name=surname, gender=gender, bornDate=dateBorn)
         new_user_info = users_info(education=education, comment=comment, citizenship=citizen)
-        db.session.add(new_user)
-        db.session.add(new_user_info)
-        db.session.commit()
+
+        Session = sessionmaker(engine)  
+        session = Session()
+
+        base.metadata.create_all(engine)
+        session.add(new_user)
+        session.add(new_user_info)
+        session.commit()
+        #db.session.add(new_user)
+        #db.session.add(new_user_info)
+        #db.session.commit()
 
 
     return render_template("picture.html")
 
 
 if __name__ == '__main__':
-    db.create_all()
+#    db.create_all()
     app.run()
